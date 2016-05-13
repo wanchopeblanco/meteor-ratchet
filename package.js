6d4bd8aba6820e0bd5f37d29294754370b46df98
Paulo Mogollon <wanchopeblanco@gmail.com>
2016-05-12 20:33:22 -0500

    Multiple enhancements
    * Removed push.js
    * Added navigation modal
    * Removed unused files
    * Changed package name & version
    * Added hammer.js
    * Added swipeBack on navigation modal

--- package.js
+++ package.js
@@ -1,29 +1,38 @@
 Package.describe({
-  name: 'pcjpcj2:ratchet',
-  summary: ' Ratchet UI Framework for Meteor',
-  version: '2.0.2',
-  git: 'https://github.com/pcjpcj2/meteor-ratchet'
+	name: 		'indesign:ratchet',
+	summary: 	' Ratchet UI Framework for Meteor without Push.js',
+	version: 	'0.0.1',
+	git: 		'https://github.com/wanchopeblanco/meteor-ratchet'
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom("0.9.3.1");
+	api.versionsFrom("0.9.3.1");
 
-  api.use('jquery');
+	api.use('jquery');
+	api.use('ecmascript');
 
-  var path = Npm.require('path');
-  var asset_path = path.join('ratchet-2.0.2');
-  api.addFiles(path.join(asset_path, 'css', 'ratchet.css'), 'client');
-  api.addFiles(path.join(asset_path, 'css', 'ratchet-theme-ios.css'), 'client');
-  api.addFiles(path.join(asset_path, 'js', 'ratchet.js'), 'client');
+	var path = Npm.require('path');
+	var asset_path = path.join('ratchet-2.0.2');
+	api.addFiles(path.join(asset_path, 'css', 'ratchet.css'), 'client');
+	api.addFiles(path.join(asset_path, 'css', 'ratchet-theme-ios.css'), 'client');
+	api.addFiles(path.join(asset_path, 'js', 'ratchet.js'), 'client');
 
-  // fonts
-  api.addFiles(path.join(asset_path, 'fonts', 'ratchicons.eot'), 'client');
-  api.addFiles(path.join(asset_path, 'fonts', 'ratchicons.ttf'), 'client');
-  api.addFiles(path.join(asset_path, 'fonts', 'ratchicons.svg'), 'client');
-  api.addFiles(path.join(asset_path, 'fonts', 'ratchicons.woff'), 'client');
+	// Themes
+	// api.addFiles(path.join(asset_path, 'css', 'ratchet-theme-android.scss'), 'client', {isImport: true});
+	// api.addFiles(path.join(asset_path, 'css', 'ratchet-theme-ios.scss'), 'client', {isImport: true});
 
-  // XXX this makes the paths to the icon sets absolute. it needs
-  // to be included _after_ the standard ratchet css so
-  // that its styles take precedence.
-  api.addFiles(path.join('ratchet-override.css'), 'client');
+	api.addFiles('./hammer/hammer.js', 'client');
+	api.addFiles('./ratchet.js', 'client');
+	api.export('Ratchet', 'client');
+
+	// fonts
+	api.addAssets(path.join(asset_path, 'fonts', 'ratchicons.eot'), 'client');
+	api.addAssets(path.join(asset_path, 'fonts', 'ratchicons.ttf'), 'client');
+	api.addAssets(path.join(asset_path, 'fonts', 'ratchicons.svg'), 'client');
+	api.addAssets(path.join(asset_path, 'fonts', 'ratchicons.woff'), 'client');
+
+	// XXX this makes the paths to the icon sets absolute. it needs
+	// to be included _after_ the standard ratchet css so
+	// that its styles take precedence.
+	api.addFiles(path.join('ratchet-override.css'), 'client');
 });
---
+++ ratchet.js
@@ -0,0 +1,59 @@
+Ratchet = {};
+
+Ratchet.navigation = function(target) {
+	const element = document.getElementById(target);
+
+	if(!element){
+		console.log('No target');
+		return;
+	}
+
+	const hammertime = new Hammer(element);
+	let isInitial = true;
+
+	hammertime.on('pan', function(event) {
+		event.preventDefault();
+
+		// Dont continue with this event if panning is not near border
+		if(isInitial && event.center.x > 30){
+			return;
+		}
+
+		// Dont continue with this event if going back more than initial x
+		if(event.deltaX < 0){
+			$(element).css('left', 'auto');
+			return;
+		}
+
+		// Dont continue with this event if panning over an image
+		if(event.target.toString() == '[object HTMLImageElement]'){
+			return;
+		}
+
+		// Set initial to false
+		isInitial = false;
+
+		// Get the distance swiped
+		const x = event.distance;
+
+		// Set the x value to the navigation modal
+		$(element).css('left', x+'px');
+
+		// If drag ended and x is over 50px go back
+		if(event.isFinal && x > 50){
+			$(element).removeClass('active');
+			setTimeout(function() {
+				$(element).css('left', 'auto');
+				window.history.back();
+			}, 200);
+
+			// If is final set initial to true again
+			isInitial = true;
+		}else if(event.isFinal){// If drag end but x is less than 50 return to left auto
+			$(element).css('left', 'auto');
+
+			// If is final set initial to true again
+			isInitial = true;
+		}
+	});
+}
